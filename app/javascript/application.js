9ec4764ccf28c0a52aa422bc0bcb89868626d8cb
Carlos Ferreira <carlosprovisorio@icloud.com>
2025-08-17 16:39:49 -0400

    fix(password reset): stable classic submit (disable Turbo on request form); token-as-id URL; tidy edit form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
-// app/javascript/application.js
-// import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-window.bootstrap = bootstrap // optional: make it available globally
+window.bootstrap = bootstrap
