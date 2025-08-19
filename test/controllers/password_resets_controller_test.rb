151ccc37ca40b42f13f464f1c5d63a2a5f6fc140
Carlos Ferreira <carlosprovisorio@icloud.com>
2025-08-18 22:42:49 -0400

    test(password_reset): align tests with token-based flow and redirect behavior

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,23 +1,51 @@
+# test/controllers/password_resets_controller_test.rb
 require "test_helper"
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get password_resets_new_url
+  setup do
+    @user = User.create!(
+      email: "carlos@example.com",
+      password: "supersecret",
+      password_confirmation: "supersecret"
+    )
+    @token = @user.signed_id(purpose: :password_reset, expires_in: 30.minutes)
+  end
+
+  test "GET new should be success" do
+    get new_password_reset_path
     assert_response :success
   end
 
-  test "should get create" do
-    get password_resets_create_url
+  test "POST create should redirect to sign_in even if email exists" do
+    post password_reset_path, params: { email: @user.email }
+    assert_redirected_to sign_in_path
+    follow_redirect!
+    assert_match "reset link", @response.body
+  end
+
+  test "GET edit with valid token should be success" do
+    get edit_password_reset_path(token: @token)
     assert_response :success
   end
 
-  test "should get edit" do
-    get password_resets_edit_url
-    assert_response :success
+  test "GET edit with invalid/expired token should redirect to new" do
+    get edit_password_reset_path(token: "bad-token")
+    assert_redirected_to new_password_reset_path
   end
 
-  test "should get update" do
-    get password_resets_update_url
-    assert_response :success
+  test "PUT update with valid token should redirect to sign_in" do
+    put password_reset_path, params: {
+      token: @token,
+      user: { password: "newsupersecret", password_confirmation: "newsupersecret" }
+    }
+    assert_redirected_to sign_in_path
+  end
+
+  test "PUT update with invalid token should redirect to new" do
+    put password_reset_path, params: {
+      token: "bad-token",
+      user: { password: "anything", password_confirmation: "anything" }
+    }
+    assert_redirected_to new_password_reset_path
   end
 end
